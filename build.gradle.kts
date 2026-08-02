f17fab3e6c078ed8a1b1bf9ac215ba26aa100ba6
Chloemlla <happyclovo@gmail.com>
2026-08-02 14:10:20 +0800

    Fix KSP version: 2.4.10-1.0.38 (2.4.10-2.0.1 does not exist)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
     id("com.android.test") version "9.3.1" apply false
     id("org.jetbrains.kotlin.android") version "2.4.10" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
-    id("com.google.devtools.ksp") version "2.4.10-2.0.1" apply false
+    id("com.google.devtools.ksp") version "2.4.10-1.0.38" apply false
     id("androidx.baselineprofile") version "1.4.1" apply false
 }
